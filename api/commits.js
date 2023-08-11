aef9900385fc52aac7aa7689091207147c689f20
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-08-11 14:10:02 -0400

    Create timeline route

--- api/commits.js
+++ api/commits.js
@@ -46,6 +46,48 @@ router.get("/count/:owner/:repo",async(req,res,next)=>{
 
 })
 
+// The following route will return a map that would contain the number of commits
+// added to a repo on a daily basis for now. The timeframe can be changed to
+// monthly basis as well. For testing purposes the current timeframe is set to a daily-basis
+
+router.get('/timeline/:owner/:repo', async (req,res,next) => {
+    try {
+        const owner = req.params.owner
+        const repo = req.params.repo
+        
+        const response = await octokit.request('GET /repos/:owner/:repo/commits', {
+            owner,
+            repo
+        })
+
+        const all_commits = response.data
+
+        // Initializing a map to store commits by Date
+        let commitsByDate = new Map()
+
+        //Iterating thru all of the commits and group them by date
+        for(currCommit of all_commits){
+            const date = new Date(currCommit.commit.author.date)
+            const key = date.toISOString().split('T')[0] // extracting the date from --> "2023-08-11T16:29:34Z"
+
+            if(commitsByDate.has(key)){
+                currCommitCount = commitsByDate.get(key)
+                commitsByDate.set(key, currCommitCount+1)
+            }else{
+                commitsByDate.set(key, 1)
+            }
+        }
+        console.log(commitsByDate)
+
+        const resMap = Object.fromEntries(commitsByDate)
+        res.json(resMap)
+
+    } catch (error) {
+        next(error)
+        
+    }
+})
+
 
 
 
