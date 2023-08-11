33e13d19f84ae55841f1e340d7fdae591c38227a
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-08-11 15:09:54 -0400

    Switch back to main db

--- db/db.js
+++ db/db.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require('sequelize');
 const {name} = require('../package.json');
 
-// const db = new Sequelize(`postgres://localhost:5432/${name}`, {
-// // const db = new Sequelize(`postgres://postgres:roman123@localhost:5432/${name}`, {
-//     logging: false,
-// });
-
-const db = new Sequelize(`postgres://localhost:5432/gitnalysis_back_end`, {
+const db = new Sequelize(`postgres://localhost:5432/${name}`, {
 // const db = new Sequelize(`postgres://postgres:roman123@localhost:5432/${name}`, {
     logging: false,
 });
+
+// const db = new Sequelize(`postgres://localhost:5432/gitnalysis_back_end`, {
+// // const db = new Sequelize(`postgres://postgres:roman123@localhost:5432/${name}`, {
+//     logging: false,
+// });
 
 module.exports = db;
