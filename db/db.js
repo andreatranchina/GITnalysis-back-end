7dad05efa7c33b1f15d02ebe859eda9c46ff07ab
Andrea <Andrea.Tranchina82@gmail.com>
2023-08-14 00:04:50 -0400

    changes for deployment

--- db/db.js
+++ db/db.js
@@ -7,6 +7,10 @@ const db = new Sequelize(process.env.POSTGRES_URL || `postgres://localhost:5432/
     logging: false,
 });
 
+// const db = new Sequelize(`postgres://localhost:5432/gitnalysis-back-end`, {
+// // const db = new Sequelize(`postgres://postgres:roman123@localhost:5432/${name}`, {
+//     logging: false,
+// });
 
 db.authenticate()
   .then(() => {
@@ -16,10 +20,7 @@ db.authenticate()
     console.error(`DB connection ${name} failed:`, error);
 });
 
-// const db = new Sequelize(`postgres://localhost:5432/gitnalysis_back_end`, {
-// // const db = new Sequelize(`postgres://postgres:roman123@localhost:5432/${name}`, {
-//     logging: false,
-// });
+
 
 
 module.exports = db;
