de5abee770dcce8959dfa6a96bf5dddf527012e7
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2023-08-15 08:36:50 -0400

    Deleting old cookie configuration

--- passport/cookieConfig.js
+++ passport/cookieConfig.js
@@ -1,13 +1,13 @@
-/**
- * Configuring the cookie session to be used by passport.js to authenticate the user. 
- * cookieConfig is imported by the `app.js` file in the root folder and used in its session initialization
- */
-const cookieConfig =
-    {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
-        secure: false, // Required to enable cookies to go through https for better security
-        httpOnly: false, // Not allowing client-side javascript to interact with cookie, thus increasing security
-        sameSite: "none", // Required to enable cors for cookies
-    };
+// /**
+//  * Configuring the cookie session to be used by passport.js to authenticate the user. 
+//  * cookieConfig is imported by the `app.js` file in the root folder and used in its session initialization
+//  */
+// const cookieConfig =
+//     {
+//         maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
+//         secure: false, // Required to enable cookies to go through https for better security
+//         httpOnly: false, // Not allowing client-side javascript to interact with cookie, thus increasing security
+//         sameSite: "none", // Required to enable cors for cookies
+//     };
 
-module.exports = cookieConfig;
+// module.exports = cookieConfig;
