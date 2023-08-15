1e5f39cf4f7b522b42e9ab914ba4e3e5647b7024
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2023-08-15 10:14:34 -0400

    Congiguring Cookies settings

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
+      secure:true,
+      sameSite:"None",
     },
   })
 );
