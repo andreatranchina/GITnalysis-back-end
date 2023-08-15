a72250de354f306c1dbd67e103830b41c687e862
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2023-08-14 22:46:17 -0400

    cookie settings

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
-      secure: false,
-      httpOnly: false,
+      secure: true,
+      httpOnly: true,
       sameSite: "none",
     },
   })
