ff8edbf0e628beb1d227b81daac6d52e87cb79be
Andrea <Andrea.Tranchina82@gmail.com>
2023-08-17 23:30:16 -0400

    attempt fix cookie issue in deployment

--- app.js
+++ app.js
@@ -31,26 +31,32 @@ app.use(cors({
   preflightContinue: true,
 }));
 
-// app.set('trust proxy', 1);
-app.enable("trust proxy");
 
-// if(process.env.BACKEND_URL==='http://localhost:8080'){
-//   app.use(
-//     session({
-//       secret: "secret",
-//       store: sessionStore,
-//       resave: false,
-//       saveUninitialized: false,
-//       cookie: {
-//         maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
-//         // sameSite:"none",
-//         // secure:true,
-//         // httpOnly:false,
-//       },
-//     })
-//   );
-// }
-// else{
+if(process.env.BACKEND_URL==='http://localhost:8080'){
+  app.enable("trust proxy");
+}
+else{
+  app.set('trust proxy', 1);
+}
+
+
+if(process.env.BACKEND_URL==='http://localhost:8080'){
+  app.use(
+    session({
+      secret: "secret",
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
+        // sameSite:"none",
+        // secure:true,
+        // httpOnly:false,
+      },
+    })
+  );
+}
+else{
   app.use(
   session({
     secret: "secret",
@@ -65,7 +71,7 @@ app.enable("trust proxy");
     },
   })
   );  
-// }
+}
 
 
 app.use(passport.initialize());
