1660d3cdda3bb955b60687784d560c475b802fbf
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-08-18 23:49:49 -0400

    edit redirect to repos page

--- api/users.js
+++ api/users.js
@@ -1,180 +1,184 @@
-const router=require("express").Router();
+const router = require("express").Router();
 const octokitMain = require("../services/octokit");
-const autheticateUser= require("../middleware/auth");
+const autheticateUser = require("../middleware/auth");
 
 // mounted on: http://localhost:8080/api/users
 
 //get authenticated user
-router.get("/me",autheticateUser,async(req,res,next)=>{
-    try {
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const response = await octokit.request('GET /user');
-        res.json(response.data);
-    } catch (error) {
-        console.log("Error in user/me® route",error)
-        next(error);
-    }
-})
+router.get("/me", autheticateUser, async (req, res, next) => {
+  try {
+    // console.log("hit auth me");
+    const octokit = octokitMain(req.user.githubAccessToken);
+    const response = await octokit.request("GET /user");
+    res.json(response.data);
+  } catch (error) {
+    console.log("Error in user/me® route", error);
+    next(error);
+  }
+});
 
 //get user info by username
-router.get("/:username",autheticateUser,async(req,res,next)=>{
-    try {
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const username = req.params.username;
+router.get("/:username", autheticateUser, async (req, res, next) => {
+  try {
+    const octokit = octokitMain(req.user.githubAccessToken);
+    const username = req.params.username;
 
-        const response = await octokit.request('GET /users/:username', {
-            username
-        });
-        
-        res.json(response.data);
-    } catch (error) {
-        console.log("Error in average route",error)
-        next(error);
-    }
-})
+    const response = await octokit.request("GET /users/:username", {
+      username,
+    });
 
+    res.json(response.data);
+  } catch (error) {
+    console.log("Error in average route", error);
+    next(error);
+  }
+});
 
 //get all followers of the authenticated user
-router.get("/me/followers",autheticateUser,async(req,res,next)=>{
-    try {
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const response = await octokit.request('GET /user/followers');
-        
-        res.json(response.data);
-    } catch (error) {
-        console.log("Error in average route",error)
-        next(error);
-    }
-})
+router.get("/me/followers", autheticateUser, async (req, res, next) => {
+  try {
+    const octokit = octokitMain(req.user.githubAccessToken);
+    const response = await octokit.request("GET /user/followers");
 
+    res.json(response.data);
+  } catch (error) {
+    console.log("Error in average route", error);
+    next(error);
+  }
+});
 
 //get list of people followed by the authenticated user
-router.get("/me/following",autheticateUser,async(req,res,next)=>{
-    try {
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const response = await octokit.request('GET /user/following');
-        
-        res.json(response.data);
-    } catch (error) {
-        console.log("Error in average route",error)
-        next(error);
-    }
-})
+router.get("/me/following", autheticateUser, async (req, res, next) => {
+  try {
+    const octokit = octokitMain(req.user.githubAccessToken);
+    const response = await octokit.request("GET /user/following");
+
+    res.json(response.data);
+  } catch (error) {
+    console.log("Error in average route", error);
+    next(error);
+  }
+});
 
 //check if a person is followed by the authenticated user
-router.get("/me/following/:username",autheticateUser,async(req,res,next)=>{
+router.get(
+  "/me/following/:username",
+  autheticateUser,
+  async (req, res, next) => {
     try {
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const username = req.params.username;
+      const octokit = octokitMain(req.user.githubAccessToken);
+      const username = req.params.username;
 
-        const response = await octokit.request('GET /user/following/:username', {
-            username
-        });
-        
-        res.json(response.data);
+      const response = await octokit.request("GET /user/following/:username", {
+        username,
+      });
+
+      res.json(response.data);
     } catch (error) {
-        console.log("Error in average route",error)
-        next(error);
+      console.log("Error in average route", error);
+      next(error);
     }
-})
+  }
+);
 
 //list of followers of a user by username
-router.get("/:username/followers",autheticateUser,async(req,res,next)=>{
-    try {
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const username = req.params.username;
+router.get("/:username/followers", autheticateUser, async (req, res, next) => {
+  try {
+    const octokit = octokitMain(req.user.githubAccessToken);
+    const username = req.params.username;
 
-        const response = await octokit.request('GET /users/:username/followers', {
-            username
-        });
-        
-        res.json(response.data);
-    } catch (error) {
-        console.log("Error in average route",error)
-        next(error);
-    }
-})
+    const response = await octokit.request("GET /users/:username/followers", {
+      username,
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.log("Error in average route", error);
+    next(error);
+  }
+});
 
 //list of people a user follows
-router.get("/:username/following",autheticateUser,async(req,res,next)=>{
-    try {
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const username = req.params.username;
+router.get("/:username/following", autheticateUser, async (req, res, next) => {
+  try {
+    const octokit = octokitMain(req.user.githubAccessToken);
+    const username = req.params.username;
 
-        const response = await octokit.request('GET /users/:username/following', {
-            username
-        });
-        
-        res.json(response.data);
-    } catch (error) {
-        console.log("Error in average route",error)
-        next(error);
-    }
-})
+    const response = await octokit.request("GET /users/:username/following", {
+      username,
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.log("Error in average route", error);
+    next(error);
+  }
+});
 
 //check if user is following another user
-router.get("/:username/following/:targetUser",autheticateUser,async(req,res,next)=>{
+router.get(
+  "/:username/following/:targetUser",
+  autheticateUser,
+  async (req, res, next) => {
     try {
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const { username, targetUser } = req.params;
+      const octokit = octokitMain(req.user.githubAccessToken);
+      const { username, targetUser } = req.params;
 
-        const response = await octokit.request('GET /users/:username/following/:targetUser', {
-            username,
-            targetUser
-        });
-        
-        res.json(response.data);
+      const response = await octokit.request(
+        "GET /users/:username/following/:targetUser",
+        {
+          username,
+          targetUser,
+        }
+      );
+
+      res.json(response.data);
     } catch (error) {
-        console.log("Error in average route",error)
-        next(error);
+      console.log("Error in average route", error);
+      next(error);
     }
-})
+  }
+);
 
 //list emails for authenticated user
-router.get("/me/emails", autheticateUser,async(req, res, next) => {
-    try {
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const response = await octokit.request('GET /user/emails');
-        
-        res.json(response.data);
-    } catch (error) {
-        console.log("Error in average route",error)
-        next(error);
-    }
+router.get("/me/emails", autheticateUser, async (req, res, next) => {
+  try {
+    const octokit = octokitMain(req.user.githubAccessToken);
+    const response = await octokit.request("GET /user/emails");
 
-})
+    res.json(response.data);
+  } catch (error) {
+    console.log("Error in average route", error);
+    next(error);
+  }
+});
 
 //list repositories for authenticated user
-router.get("/me/repos",autheticateUser, async(req, res, next) => {
-    try {
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const response = await octokit.request('GET /user/repos');
-        res.json(response.data);
-    } catch (error) {
-        console.log("Error in average route",error)
-        next(error);
-    }
-
-})
-
+router.get("/me/repos", autheticateUser, async (req, res, next) => {
+  try {
+    const octokit = octokitMain(req.user.githubAccessToken);
+    const response = await octokit.request("GET /user/repos");
+    res.json(response.data);
+  } catch (error) {
+    console.log("Error in average route", error);
+    next(error);
+  }
+});
 
 //list repositories for of a user by username
-router.get("/:username/repos",autheticateUser, async(req, res, next) => {
-    try {
-        const username = req.params.username;
-        const octokit =  octokitMain(req.user.githubAccessToken)
-        const response = await octokit.request('GET /users/:username/repos', {
-            username
-        });
-        
-        res.json(response.data);
-    } catch (error) {
-        console.log("Error in average route",error)
-        next(error);
-    }
+router.get("/:username/repos", autheticateUser, async (req, res, next) => {
+  try {
+    const username = req.params.username;
+    const octokit = octokitMain(req.user.githubAccessToken);
+    const response = await octokit.request("GET /users/:username/repos", {
+      username,
+    });
 
-})
+    res.json(response.data);
+  } catch (error) {
+    console.log("Error in average route", error);
+    next(error);
+  }
+});
 
-
-
-module.exports=router;
+module.exports = router;
--- app.js
+++ app.js
@@ -1,46 +1,43 @@
 //./app.js
 require("dotenv").config();
 
-
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser'); //may or may not need
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser"); //may or may not need
 const app = express();
 
 //Database imports
-const passport = require('passport');
-const db = require('./db');
+const passport = require("passport");
+const db = require("./db");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sessionStore = new SequelizeStore({ db });
 
-
 const PORT = 8080;
 
-//setup middleware 
+//setup middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.enable("trust proxy",true);
-app.use(cors({
-  //production front end url
-  origin: process.env.FRONTEND_URL, // allow to server to accept request from different origin
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  allowedHeaders:
-  "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  preflightContinue: true,
-}));
+app.use(express.urlencoded({ extended: true }));
+app.enable("trust proxy", true);
+app.use(
+  cors({
+    //production front end url
+    origin: process.env.FRONTEND_URL, // allow to server to accept request from different origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders:
+      "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    preflightContinue: true,
+  })
+);
 
-
-if(process.env.BACKEND_URL==='http://localhost:8080'){
+if (process.env.BACKEND_URL === "http://localhost:8080") {
   app.enable("trust proxy");
-}
-else{
-  app.set('trust proxy', 1);
+} else {
+  app.set("trust proxy", 1);
 }
 
-
-if(process.env.BACKEND_URL==='http://localhost:8080'){
+if (process.env.BACKEND_URL === "http://localhost:8080") {
   app.use(
     session({
       secret: "secret",
@@ -55,61 +52,60 @@ if(process.env.BACKEND_URL==='http://localhost:8080'){
       },
     })
   );
-}
-else{
+} else {
   app.use(
-  session({
-    secret: "secret",
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
-      sameSite:"none",
-      secure:true,
-      httpOnly:false,
-    },
-  })
-  );  
+    session({
+      secret: "secret",
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
+        sameSite: "none",
+        secure: true,
+        httpOnly: false,
+      },
+    })
+  );
 }
-
 
 app.use(passport.initialize());
 app.use(passport.session());
-require("./passport/passportConfig")(passport)
-
+require("./passport/passportConfig")(passport);
 
 //hiting root route
 app.get("/", (req, res, next) => {
-    res.send("Hitting backend root success!")
-})
-
-
-//mounted on `localhost:8080/github`
-app.get('/github/auth',passport.authenticate('github',{ scope: [ 'user', 'repo' ] }));
+  res.send("Hitting backend root success!");
+});
 
 //mounted on `localhost:8080/github`
-app.get('/github/auth/callback',
-  passport.authenticate('github', {
-    failureRedirect: '/auth/error'
+app.get(
+  "/github/auth",
+  passport.authenticate("github", { scope: ["user", "repo"] })
+);
+
+//mounted on `localhost:8080/github`
+app.get(
+  "/github/auth/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/auth/error",
   }),
-  (req, res,next) => {
+  (req, res, next) => {
     // Successful authentication, log the user in
-    const user=req.user
-    req.logIn(user, function(err) {
+    const user = req.user;
+    req.logIn(user, function (err) {
       if (err) {
         return next(err);
       }
       // res.send("User Logged IN")
       // Construct the redirect URL with the repository name as a query parameter
-      const redirectUrl = `${process.env.FRONTEND_URL}/?username=${user.username}&userId=${user.githubID}`;
+      const redirectUrl = `${process.env.FRONTEND_URL}/repos/?username=${user.username}&userId=${user.githubID}`;
       return res.redirect(redirectUrl); // Redirect to your desired URL
     });
   }
 );
 
-
-app.get('/github/logout', async (req, res, next) => {
+app.get("/github/logout", async (req, res, next) => {
   req.logout((err) => {
     if (err) {
       return next(err);
@@ -128,35 +124,33 @@ app.get('/github/logout', async (req, res, next) => {
   });
 });
 
-
-
 //mounting on routes
-app.use('/api', require('./api'));
+app.use("/api", require("./api"));
 
 // 404 Handling - This route should be at the end to handle unknown routes
 app.use((error, req, res, next) => {
-    res.status(error.status || 500).send(error.message);
+  res.status(error.status || 500).send(error.message);
 });
 
 //Run server function
 const serverRun = () => {
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-  })
-}
+  });
+};
 
 //aggregated all functions needed to be invoked before running the server
 async function main() {
   console.log("This is going to print models: ", db.models);
   //syncing DB function
   // use {force: true} to drop the tables and starts from scratch (then re-seed)
-    // await db.sync( {force: true });
-    await db.sync();
-    await sessionStore.sync();
-    serverRun();
+  // await db.sync( {force: true });
+  await db.sync();
+  await sessionStore.sync();
+  serverRun();
 }
 
 //invoking all functions
-main()
+main();
 
 module.exports = app;
