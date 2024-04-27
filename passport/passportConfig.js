add98e6af5eba50cfd4b0edb6c19dfeb23d81bc1
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2024-04-27 11:29:04 -0400

    update deserialize and octokit initialization

--- passport/passportConfig.js
+++ passport/passportConfig.js
@@ -1,57 +1,57 @@
 //./passport/passPortconfig.js
-const User = require("../db/models/user")
-const GitHubStrategy = require('passport-github2').Strategy;
+const User = require("../db/models/user");
+const GitHubStrategy = require("passport-github2").Strategy;
 require("dotenv").config();
 
-module.exports =  (passport) =>{
-  
-  passport.serializeUser((user, done) =>{
-    console.log("Serialized User")
+module.exports = (passport) => {
+  passport.serializeUser((user, done) => {
+    console.log("Serialized User");
     done(null, user.githubID);
   });
-  
+
   passport.deserializeUser(async (id, done) => {
-  console.log("User id in deserializeUser: ", id);
-  try {
-    const user = await User.findByPk(id);
-    done(null, user);
-  } catch (error) {
-    done(error);
-  }
-});
-  
-  
-  passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    // callbackURL: `${process.env.FRONTEND_URL}/github/auth/callback` || "localhost:8080/github/auth/callback"
-    callbackURL:`${process.env.BACKEND_URL}/github/auth/callback`
-  },
-  
-  async function(accessToken, refreshToken, profile, done) {
+    console.log("User id in deserializeUser: ", id);
     try {
-      const user = await User.findOne({where:{githubID:profile.id}})
-      console.log(profile,'is the full profile')
-      console.log(profile.username,'is the username')
-      console.log(profile.displayName,'isthe displayname')
-      console.log(profile.id,'is the github id')
-      // console.log(profile)
-      if (!user){
-        const newUser=await User.create({
-          githubID:profile.id,
-          githubAccessToken:accessToken,
-          username:profile.username,
-          fullname:profile.displayName,
-          profilePhoto: profile._json.avatar_url
-        })
-        return done(null,newUser)
-      }
-      return done(null, user);
-      
+      const user = await User.findByPk(id);
+      return user ? done(null, user) : done(null, null);
     } catch (error) {
-      console.log("Error in Stategy")
+      done(error, null);
     }
-  
-  }
-  ));
-}
+  });
+
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        // callbackURL: `${process.env.FRONTEND_URL}/github/auth/callback` || "localhost:8080/github/auth/callback"
+        callbackURL: `${process.env.BACKEND_URL}/github/auth/callback`,
+      },
+
+      async function (accessToken, refreshToken, profile, done) {
+        try {
+          const user = await User.findOne({ where: { githubID: profile.id } });
+          console.log(profile, "is the full profile");
+          console.log(profile.username, "is the username");
+          console.log(profile.displayName, "isthe displayname");
+          console.log(profile.id, "is the github id");
+          console.log(profile._json.avatar_url, "is the profile photo");
+          // console.log(profile)
+          if (!user) {
+            const newUser = await User.create({
+              githubID: profile.id,
+              githubAccessToken: accessToken,
+              username: profile.username,
+              fullname: profile.displayName,
+              profilePhoto: profile._json.avatar_url,
+            });
+            return done(null, newUser);
+          }
+          return done(null, user);
+        } catch (error) {
+          console.log("Error in Stategy");
+        }
+      }
+    )
+  );
+};
--- services/octokit.js
+++ services/octokit.js
@@ -1,14 +1,13 @@
-const {Octokit}=require('@octokit/rest')
+const { Octokit } = require("@octokit/rest");
 
-module.exports = (access_token)=>{
-
-    //if the method is called with an access token, create an axios header
-    return access_token?new Octokit({ 
-        userAgent: {"User-Agent": "GITNALYSIS/1.0"},
-        auth: access_token 
-        //if the method is called without an access_token, return an empty header
-    }):new Octokit({ 
-        userAgent: {"User-Agent": "GITNALYSIS/1.0"},
-    })
-    
+module.exports = (access_token) => {
+  // If the method is called with an access token, create an axios header
+  return access_token
+    ? new Octokit({
+        userAgent: "GITNALYSIS/1.0",
+        auth: access_token,
+      })
+    : new Octokit({
+        userAgent: "GITNALYSIS/1.0",
+      });
 };
