5ebcc5c4308af60c8c27b64634f705cfb34e7951
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2024-04-27 12:38:54 -0400

    put logging middleware before cors

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 // app.enable("trust proxy",true);
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url);
+  console.log("Request headers:", req.headers);
+  next();
+});
 app.use(
   cors({
     //production front end url
@@ -133,12 +138,6 @@ app.use("/api", require("./api"));
 // 404 Handling - This route should be at the end to handle unknown routes
 app.use((error, req, res, next) => {
   res.status(error.status || 500).send(error.message);
-});
-
-app.use((req, res, next) => {
-  console.log("Incoming request:", req.method, req.url);
-  console.log("Request headers:", req.headers);
-  next();
 });
 
 //Run server function
